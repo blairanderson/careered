739b8cd6c8ab6f3e22e7ff19324cf72a01e912ed
Blair Anderson <blair@repairshopr.com>
2015-03-21 13:10:06 -0700

    delete ember_client

--- ember_client/app/adapters/application.js
+++
@@ -1,8 +0,0 @@
-import DS from 'ember-data';
-import $ from 'jquery';
-
-export default DS.RESTAdapter.extend({
-  headers: {
-    "X-CSRF-Token": $('meta[name="csrf-token"]').attr('content')
-  }
-});
