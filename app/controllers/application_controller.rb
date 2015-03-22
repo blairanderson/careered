4cafc0483aec3b3d6a048a4a8ccc6b3fcdec646c
Blair Anderson <blair@repairshopr.com>
2015-03-22 00:55:00 -0700

    add ember redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,19 @@ class ApplicationController < ActionController::Base
 
   include SessionsHelper
 
+  def not_found
+    if current_user && board = current_user.boards.find(params[:id])
+      redirect_to "#{ember_path}?board_id=#{board.id}"
+    else
+      redirect_to root_path
+    end
+  end
+
   def redirect_logged_in_user
     if logged_in?
       redirect_to root_url
     end
   end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root to: "root#index"
   get "ember" => "root#ember"
+  get "ember/:id" => "root#ember"
   get "react" => "root#react"
+  get "react/:id" => "root#react"
 
   resource :session, only: [:new, :create, :destroy]
   get "login" => "sessions#new"
@@ -9,28 +11,27 @@ Rails.application.routes.draw do
 
 
   namespace :api do
+    resources :api_keys
     resources :users, only: [:show] do
       collection do
         get :current
       end
     end
 
-    resources :api_keys
-
     resources :boards, only: [:index, :show, :create, :update]
-
     resources :lists, except: [:edit] do
       collection do
         post :sort
       end
     end
-
     resources :cards, except: [:edit] do
       collection do
         post :sort
       end
     end
-
     resources :card_comments, only: [:index, :create, :destroy]
   end
+
+  get ":id" => "application#not_found", as: :not_found
+  get "*/*" => "application#not_found"
 end
