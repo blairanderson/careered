ec6fb4acf0e9948d55b1469ae9301ce1e4cb8b0a
Blair Anderson <blair@repairshopr.com>
2015-03-21 18:22:02 -0700

    start to work through react

--- app/assets/javascripts/components/application.js.jsx
+++ app/assets/javascripts/components/application.js.jsx
@@ -1,31 +1,75 @@
+var { Route, DefaultRoute, RouteHandler, Link } = ReactRouter;
+var data = window.data;
+
 var Application = React.createClass({
-  //mixins: [FluxMixin, StoreWatchMixin("BoardStore")],
-  propTypes: {
-    currentUser: React.PropTypes.object
+  getInitialState: function () {
+    return {
+      user: data.user,
+      boards: data.boards,
+      lists: data.lists,
+      cards: data.cards
+    };
   },
 
   render: function() {
-    var user = JSON.parse(this.props.current_user);
-    this.props.user = {
-      name: user.name,
-      bio: user.bio,
-      email: user.email,
-      full_name: user.full_name
-    };
-    this.props.boards = user.boards;
-    this.props.cards = user.cards;
-    this.props.lists = user.lists;
-    return (<div>
-      <div>Current User: {this.props.user}</div>
-      <ul>
-        {this.props.boards.map(function(board, i) {
-          var lists = this.props.lists.filter(function(list){
-            return list.board_id === board.id;
-          });
-          return <li key={i}><Board board={board} lists={lists} /></li>;
-        }.bind(this))}
-      </ul>
+    var links = this.state.boards.map(function(board, i) {
+      return (
+              <li key={board.slug}>
+                <Link
+                    to="board"
+                    params={{slug: board.slug}}
+                    >{board.title}</Link>
+              </li>
+            );
+    });
+    return (
+      <div>
+        <div>Current User: {this.state.user.full_name}</div>
+        <ul> {links} </ul>
+        <div class="detail">
+          <RouteHandler/>
+        </div>
       </div>
     );
   }
+});
+
+var Index = React.createClass({
+  render: function () {
+    return <h1>Select a board!</h1>;
+  }
+});
+
+function findBoard(slug){
+  return window.data.boards.find(function(board){
+    return board.slug === slug;
+  });
+}
+
+var Board = React.createClass({
+  contextTypes: {
+    router: React.PropTypes.func.isRequired
+  },
+
+  render: function () {
+    var slug = this.context.router.getCurrentParams().slug;
+    var board = findBoard(slug);
+    debugger
+    return (
+      <div className="board">
+        <h1>{board.title}</h1>
+      </div>
+    );
+  }
+});
+
+var routes = (
+  <Route name="app" path={window.location.pathname} handler={Application}>
+    <DefaultRoute handler={Index}/>
+    <Route name="board" path="board/:slug" handler={Board}/>
+  </Route>
+);
+
+ReactRouter.run(routes, function (Handler) {
+  React.render(<Handler/>, document.getElementById('main'));
 });
